5ad5157fb7988e27a0a5cb12c6f11eed11361d3c
Colin Canaday <cjcanaday@gmail.com>
2025-03-22 00:00:00 +1735

    Implemented ray-sphere intersection logic

--- src/geometries/GeomSphere.cpp
+++ src/geometries/GeomSphere.cpp
@@ -7,39 +7,48 @@
 #include "Ray.h"
 
 std::vector<Intersection> GeomSphere::intersect(Ray &ray) {
-    /**
-     * NOTE: Ray is already transformed to the Model coordinate space.
-     */
     using namespace glm;
 
     // vector to store the intersections
     std::vector<Intersection> intersections;
 
-    /**
-     * TODO: Implement the Ray intersection with the current geometry
-     */
+    vec3 ro = ray.p0; // ray origin
+    vec3 rd = normalize(ray.dir); // ray dir normalized
 
-    /**
-     * Once you find the intersection, add it to the `intersections` vector.
-     *
-     * Example:
-     * Suppose the ray intersects the current geometry at a point `vec3 point`
-     * at a distance `float t`, and the unit normal vector at the intersection
-     * point is `vec3 normal`. You would then insert an intersection into the
-     * vector as follows:
-     *
-     * intersections.push_back({t, point, normal, this, nullptr});
-     *
-     * Note:
-     * - Here we pass the Model pointer as `nullptr` because it will be
-     *   populated by the Model::intersect() function call.
-     * - Only add intersections that are in front of the camera, i.e.,
-     *   where t > 0.
-     */
+    vec3 c = this->center; // center
+    float r = this->radius; // radius
 
-    /**
-     * TODO: Update `intersections`
-     */
+    vec3 oc = ro - c;
+    float B = dot(rd, oc);
+    float discriminant = (B * B) - dot(oc, oc) + (r * r); // slide 64
+    if (discriminant < 0) { // if negative then no intersection; 
+        return intersections;
+    }
+
+    float sqrt_discriminant = sqrt(discriminant);
+    float t1 = (-B + sqrt_discriminant);
+    float t2 = (-B - sqrt_discriminant);
+
+    float t;
+
+
+    // only want to consider intersections in front of camera (pos t vals)
+    if (t1 > 0 && t2 > 0) {
+        t = std::min(t1, t2);  // Take smallest pos t
+    }
+    else if (t1 > 0) {
+        t = t1;  // Only t1 is valid
+    }
+    else if (t2 > 0) {
+        t = t2;  // Only t2 is valid
+    }
+    else {
+        return intersections;  // Both are behind the camera
+    }
+
+    vec3 point = ro + t * rd;
+    vec3 normal = normalize(point - center);
+    intersections.push_back({ t, point, normal, this, nullptr });
 
     return intersections;
 };
